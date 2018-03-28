9fe5f5a6cd7db8c9625a2f15ca41714aa8b78076
erik lundstedt <minecraft.erilun06@gmail.com>
2018-03-28 13:46:03 +0200

    upp

--- 8957678324385235524455358874827933866245672397697442767586337895266557943422925874569322722635364763/js/navbar.js
+++ 8957678324385235524455358874827933866245672397697442767586337895266557943422925874569322722635364763/js/navbar.js
@@ -1,48 +1,29 @@
 
 window.onscroll = function() {myFunction2()};
-var toggle=0;
 
 function myFunction2() {
 	var navbar = document.getElementById("navbar");
 	var sticky = navbar.offsetTop;
 	var classes=navbar.classList;
+	var x=230;
   if (window.pageYOffset >= 230) {//230/sticky
 		navbar.classList.add("sticky")
-		console.log(classes);
 		console.log(window.pageYOffset+". . . ."+sticky);
   } else if (window.pageYOffset <= 230){//230/sticky
 		navbar.classList.remove("sticky");
-		console.log(classes);
 		console.log(window.pageYOffset+". . . ."+sticky);
   }
 }
 
-/*
-function functionName() {
-	var navbar = document.getElementById("navbar");
-	if (toggle==1) {
-		navbar.classList.add("sticky")
-		console.log(toggle);
-		toggle=0;
-	}
-	 else if (toggle==0)
-	{
-		navbar.classList.remove("sticky");
-		console.log(toggle);
-		toggle=1;
-	}
-}
-
-
-*/
-
-
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-	var x = document.getElementById("navbar");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
+    var x = document.getElementById("navbar");
+		var classes=x.classList;
+    if (x.className == "topnav") {
+        x.className += " responsive";
+				console.log(classes);
+    } else {
+        x.className = "topnav";
+				console.log(classes);
+    }
 }
