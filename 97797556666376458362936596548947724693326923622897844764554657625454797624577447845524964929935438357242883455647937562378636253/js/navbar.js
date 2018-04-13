d5bd3094b510cd420374df92d2d0b868b9d7b5f2
erik lundstedt <minecraft.erilun06@gmail.com>
2018-04-13 12:29:40 +0200

    upp

---
+++ 97797556666376458362936596548947724693326923622897844764554657625454797624577447845524964929935438357242883455647937562378636253/js/navbar.js
@@ -0,0 +1,59 @@
+
+
+window.onscroll = function() {myFunction2()};
+
+function myFunction2() {
+	var navbar = document.getElementById("navbar");
+	var sticky = navbar.offsetTop;
+	var classes=navbar.classList;
+	var x=210;//230
+	if (window.pageYOffset >= 284) {//230/sticky284
+		navbar.classList.add("sticky")
+		console.log(window.pageYOffset+". . . ."+sticky);
+	} else if (window.pageYOffset <= 284){//230/sticky
+		navbar.classList.remove("sticky");
+		console.log(window.pageYOffset+". . . ."+sticky);
+	}
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
+function myFunction() {
+	var x = document.getElementById("navbar");
+	var classes=x.classList;
+	console.log("........."+x.className);
+	if (x.classList === "topnav active")
+	{
+		x.classList.remove("active")
+		console.log("inactive");
+	}
+else if (x.classlist==="topnav sticky active")
+{
+	x.classList.remove("active")
+	console.log("inactive");
+}
+	else
+	{
+		x.classList.add("active")
+		console.log("active");
+	}
+
+		}
+	}
+}
+
+
+
+var state=false
+function stylechange()
+{
+	var x = document.getElementById("style");
+	if (state===true) {
+		x.className=("spacer loggocol")
+		state=false
+	} else if(state===false){
+		x.className=("spacer white")
+		state=true
+	}
+//	x.classList.exsists()
+}
