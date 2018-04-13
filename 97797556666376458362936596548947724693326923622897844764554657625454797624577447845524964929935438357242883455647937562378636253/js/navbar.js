085dc59708ba45d2f14c8cbeebc04bff55307477
erik lundstedt <minecraft.erilun06@gmail.com>
2018-04-13 13:24:37 +0200

    try fix

--- 97797556666376458362936596548947724693326923622897844764554657625454797624577447845524964929935438357242883455647937562378636253/js/navbar.js
+++ 97797556666376458362936596548947724693326923622897844764554657625454797624577447845524964929935438357242883455647937562378636253/js/navbar.js
@@ -15,7 +15,6 @@ function myFunction2() {
 		console.log(window.pageYOffset+". . . ."+sticky);
 	}
 }
-
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 
 function myFunction() {
@@ -37,7 +36,6 @@ function myFunction() {
 		x.classList.add("active")
 		console.log("active");
 	}
-
 }
 
 
