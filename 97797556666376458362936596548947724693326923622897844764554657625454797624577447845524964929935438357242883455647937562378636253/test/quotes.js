d6c99b2f182bde4601b4034c1e0697f39243038f
erik lundstedt <minecraft.erilun06@gmail.com>
2018-04-19 12:33:50 +0200

    upp test

---
+++ 97797556666376458362936596548947724693326923622897844764554657625454797624577447845524964929935438357242883455647937562378636253/test/quotes.js
@@ -0,0 +1,28 @@
+//var showQ = document.getElementById("quote");
+
+
+function initAPI()
+{
+	loadFromAPI(function(response)
+	{
+		// Parse JSON string into object
+		var actual_JSON = JSON.parse(response);
+		console.log(actual_JSON);
+	}
+);
+}
+
+
+function loadFromAPI(callback)
+{
+	var quote = new XMLHttpRequest();
+	quote.overrideMimeType("application/json");
+	quote.open('GET', 'quotes.json', true); // Replace 'my_data' with the path to your file
+	quote.onreadystatechange = function () {
+		if (quote.readyState == 4 && quote.status == "200") {
+			// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+			callback(quote.responseText);
+		}
+	};
+	quote.send(null);
+}
