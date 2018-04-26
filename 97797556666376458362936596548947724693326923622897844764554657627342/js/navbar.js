c4ff1eaa3fd0f50842e2cb46a760a76e3c61d2c5
erik lundstedt <minecraft.erilun06@gmail.com>
2018-04-26 09:53:53 +0200

    upp

--- 97797556666376458362936596548947724693326923622897844764554657627342/js/navbar.js
+++ 97797556666376458362936596548947724693326923622897844764554657627342/js/navbar.js
@@ -1,22 +1,52 @@
+//document.getElementById("luxbar").style.padding="50px 0px";
 
 
-window.onscroll = function() {myFunction2()};
 
-function myFunction2() {
-	var navbar = document.getElementById("navbar");
+//window.onscroll = function() {myFunction()};
+window.onscroll = function() {fun2()};
+
+function myFunction() {
+	var navbar = document.getElementById("luxbar");
 	var sticky = navbar.offsetTop;
 	var classes = navbar.classList;
 	var x=210;//230
 	if (window.pageYOffset >= 284) {//230/sticky284
 		navbar.classList.add("sticky")
-		console.log(window.pageYOffset+". . . ."+sticky);
+		console.log(window.pageYOffset+". . . ."+sticky+" true");
 	} else if (window.pageYOffset <= 284){//230/sticky
 		navbar.classList.remove("sticky");
-		console.log(window.pageYOffset+". . . ."+sticky);
+		console.log(window.pageYOffset+". . . ."+sticky+" false");
 	}
 }
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 
+function fun2()
+{
+	var topnav = document.getElementById("luxbar");
+	var navbar = document.getElementsByClassName("luxbar-navigation");
+	var sticky = topnav.offsetTop;
+	var classes = navbar.classList;
+	var x=270;//250
+	if (window.pageYOffset<=2) {
+		topnav.style.margin="0px 0px 0px";
+	} else {
+		topnav.style.padding= x-window.pageYOffset-5+"px 0px 0px";
+	}
+
+	console.log(sticky+"...padding: "+topnav.style.padding+"...offset: "+window.pageYOffset);
+}
+
+
+function main()
+{
+	var topnav = document.getElementById("luxbar");
+	var sticky = topnav.offsetTop;
+	navbar.style.padding=window.pageYOffset+"252px 0px";
+	console.log("123");
+}
+main();
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/*
 function myFunction() {
 	var x = document.getElementById("navbar");
 	var classes=x.classList;
@@ -36,7 +66,7 @@ function myFunction() {
 		x.classList.add("active")
 		console.log("active");
 	}
-}
+}*/
 
 
 
