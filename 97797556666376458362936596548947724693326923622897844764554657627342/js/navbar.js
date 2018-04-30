5a99e81361f6f842dc0b95e3b7fc4b467713c5b0
erik lundstedt <minecraft.erilun06@gmail.com>
2018-04-30 15:04:40 +0200

    upp

--- 97797556666376458362936596548947724693326923622897844764554657627342/js/navbar.js
+++ 97797556666376458362936596548947724693326923622897844764554657627342/js/navbar.js
@@ -1,91 +1,112 @@
-//document.getElementById("luxbar").style.padding="50px 0px";
-
-
-
-//window.onscroll = function() {myFunction()};
-window.onscroll = function() {fun2()};
-
-function myFunction() {
-	var navbar = document.getElementById("luxbar");
-	var sticky = navbar.offsetTop;
-	var classes = navbar.classList;
-	var x=210;//230
-	if (window.pageYOffset >= 284) {//230/sticky284
-		navbar.classList.add("sticky")
-		console.log(window.pageYOffset+". . . ."+sticky+" true");
-	} else if (window.pageYOffset <= 284){//230/sticky
-		navbar.classList.remove("sticky");
-		console.log(window.pageYOffset+". . . ."+sticky+" false");
+var TgTableSort = window.TgTableSort || function(n, t) {
+	"use strict";
+	function r(n, t) {
+		for (var e = [], o = n.childNodes, i = 0; i < o.length; ++i) {
+			var u = o[i];
+			if ("." == t.substring(0, 1)) {
+				var a = t.substring(1);
+				f(u, a) && e.push(u);
+			} else u.nodeName.toLowerCase() == t && e.push(u);
+			var c = r(u, t);
+			e = e.concat(c);
+		}
+		return e;
 	}
-}
-
-function fun2()
-{
-	var topnav = document.getElementById("luxbar");
-	var navbar = document.getElementsByClassName("luxbar-navigation");
-	var sticky = topnav.offsetTop;
-	var classes = navbar.classList;
-	var x=290;//250
-	if (window.pageYOffset<=0.999999999999999999) {
-		topnav.style.margin="0px 0px 0px";
-		//topnav.style.backgroundAttachment="fixed";
-	} else {
-		topnav.style.padding= x-window.pageYOffset-5+"px 0px 0px";
-		//topnav.style.backgroundAttachment="scroll";
+	function e(n, t) {
+		var e = [], o = r(n, "tr");
+		return o.forEach(function(n) {
+			var o = r(n, "td");
+			t >= 0 && t < o.length && e.push(o[t]);
+		}), e;
 	}
-
-	console.log(sticky+"...padding: "+topnav.style.padding+"...offset: "+window.pageYOffset);
-}
-
-
-function main()
-{
-	var topnav = document.getElementById("luxbar");
-	var sticky = topnav.offsetTop;
-	topnav.style.padding=window.pageYOffset+"252px 0px";
-	console.log("123");
-}
-//main();
-fun2();
-/*
-Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
-*/
-/*
-function myFunction() {
-var x = document.getElementById("navbar");
-var classes=x.classList;
-console.log("........."+x.className);
-if (x.classList === "topnav active")
-{
-x.classList.remove("active")
-console.log("inactive");
-}
-else if (x.classlist==="topnav sticky active")
-{
-x.classList.remove("active")
-console.log("inactive");
-}
-else
-{
-x.classList.add("active")
-console.log("active");
-}
-}
-*/
-
-
-
-
-var state=false
-function stylechange()
-{
-	var x = document.getElementById("style");
-	if (state===true) {
-		x.className=("spacer loggocol")
-		state=false
-	} else if(state===false){
-		x.className=("spacer white")
-		state=true
+	function o(n) {
+		return n.textContent || n.innerText || "";
 	}
-	//	x.classList.exsists()
-}
+	function i(n) {
+		return n.innerHTML || "";
+	}
+	function u(n, t) {
+		var r = e(n, t);
+		return r.map(o);
+	}
+	function a(n, t) {
+		var r = e(n, t);
+		return r.map(i);
+	}
+	function c(n) {
+		var t = n.className || "";
+		return t.match(/\S+/g) || [];
+	}
+	function f(n, t) {
+		return -1 != c(n).indexOf(t);
+	}
+	function s(n, t) {
+		f(n, t) || (n.className += " " + t);
+	}
+	function d(n, t) {
+		if (f(n, t)) {
+			var r = c(n), e = r.indexOf(t);
+			r.splice(e, 1), n.className = r.join(" ");
+		}
+	}
+	function v(n) {
+		d(n, L), d(n, E);
+	}
+	function l(n, t, e) {
+		r(n, "." + E).map(v), r(n, "." + L).map(v), e == T ? s(t, E) : s(t, L);
+	}
+	function g(n) {
+		return function(t, r) {
+			var e = n * t.str.localeCompare(r.str);
+			return 0 == e && (e = t.index - r.index), e;
+		};
+	}
+	function h(n) {
+		return function(t, r) {
+			var e = +t.str, o = +r.str;
+			return e == o ? t.index - r.index : n * (e - o);
+		};
+	}
+	function m(n, t, r) {
+		var e = u(n, t), o = e.map(function(n, t) {
+			return {
+				str: n,
+				index: t
+			};
+		}), i = e && -1 == e.map(isNaN).indexOf(!0), a = i ? h(r) : g(r);
+		return o.sort(a), o.map(function(n) {
+			return n.index;
+		});
+	}
+	function p(n, t, r, o) {
+		for (var i = f(o, E) ? N : T, u = m(n, r, i), c = 0; t > c; ++c) {
+			var s = e(n, c), d = a(n, c);
+			s.forEach(function(n, t) {
+				n.innerHTML = d[u[t]];
+			});
+		}
+		l(n, o, i);
+	}
+	function x(n, t) {
+		var r = t.length;
+		t.forEach(function(t, e) {
+			t.addEventListener("click", function() {
+				p(n, r, e, t);
+			}), s(t, "tg-sort-header");
+		});
+	}
+	var T = 1, N = -1, E = "tg-sort-asc", L = "tg-sort-desc";
+	return function(t) {
+		var e = n.getElementById(t), o = r(e, "tr"), i = o.length > 0 ? r(o[0], "td") : [];
+		0 == i.length && (i = r(o[0], "th"));
+		for (var u = 1; u < o.length; ++u) {
+			var a = r(o[u], "td");
+			if (a.length != i.length) return;
+		}
+		x(e, i);
+	};
+}(document);
+
+document.addEventListener("DOMContentLoaded", function(n) {
+	TgTableSort("tg-m07a5");
+});
