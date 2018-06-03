d4499173c1cae28b747015b75590f758d3cca9d9
erik lundstedt <minecraft.erilun06@gmail.com>
2018-06-03 20:16:26 +0200

    fixes
    text fixes

--- 97797556666376458362936596548947724693326923622897844764554657627342/js/master.js
+++ 97797556666376458362936596548947724693326923622897844764554657627342/js/master.js
@@ -1,1 +1,11 @@
-/**/
+function timesOn()
+{
+  var quote=document.getElementById("qoute");
+    quote.classList.add("times");
+    console.log("PÅ");
+}
+function timesOff() {
+  var quote = document.getElementById("qoute");
+    quote.classList.remove("times");
+    console.log("av");
+}
